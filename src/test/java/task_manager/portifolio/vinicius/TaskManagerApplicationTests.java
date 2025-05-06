25e784dca6a7662ecc4ca5691b2ccb0f8038a5cc
Vinicius Patricio <viniciuspatricio.33@gmail.com>
2025-05-06 11:23:37 -0300

    Configurações inicias do Task Manager

---
+++ src/test/java/task_manager/portifolio/vinicius/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package task_manager.portifolio.vinicius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
